b2023ed8af7a9b92a15e208ec8a5306cd564285f
David Conner <dconner.pro@gmail.com>
2019-09-11 11:11:57 -0400

    gen-vsop.js: stubbed out script to generate GeoTIFF texture from VSOP data
    * running into issues with .babelrc (... never used babel)

---
+++ script/gen-vsop.js
@@ -0,0 +1,12 @@
+// import GeoTIFF from 'geotiff';
+const GeoTIFF = require('geotiff');
+
+// console.log(process.env.NODE_OPTIONS);
+
+// TODO: debugging with npm doesn't work using NODE_OPTIONS
+
+const VSOP = require('astronomia').data;
+var foo = 'bar';
+foo = 'baz';
+
+// console.log(VSOP);
