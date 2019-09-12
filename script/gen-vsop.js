f3bee21bac31db3d242bc446c50165501b0d2ea8
David Conner <dconner.pro@gmail.com>
2019-09-12 12:41:32 -0400

    add @babel/plugin-transform-runtime and the old babel-runtime (for GeoTIFF)

--- script/gen-vsop.js
+++ script/gen-vsop.js
@@ -1,12 +1,5 @@
 // import GeoTIFF from 'geotiff';
 const GeoTIFF = require('geotiff');
-
-// console.log(process.env.NODE_OPTIONS);
-
-// TODO: debugging with npm doesn't work using NODE_OPTIONS
-
 const VSOP = require('astronomia').data;
-var foo = 'bar';
-foo = 'baz';
 
 // console.log(VSOP);
