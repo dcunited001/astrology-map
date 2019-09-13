44856d2d590f12577c7f53670d11874b58cdb822
David Conner <dconner.pro@gmail.com>
2019-09-13 02:44:06 -0400

    VSOP: trying to figure out how to store data and how to calculate in realtime
    * Geo/TIFF with custom fields?
    
    * GPU mapreduce? Use Tensorflow?
    
    * Custom shader for `(A * cos(B + C*T) * T^n)`?  Only use TFJS for the mapreduce?
    
    * To calculate both pos/vel in one step?
    
    * Too large to fold multiple T calcs in one render call?
    
    * how does a single reshape on a 512x512 texture before sum affect performance in TF?

--- script/gen-vsop.js
+++ script/gen-vsop.js
@@ -2,4 +2,56 @@
 const GeoTIFF = require('geotiff');
 const VSOP = require('astronomia').data;
 
-// console.log(VSOP);
+const planets = [
+  'mercury',
+  'venus',
+  'earth',
+  'mars',
+  'jupiter',
+  'saturn',
+  'uranus',
+  'neptune'
+]
+
+const heliocentricKeys = ['L', 'B', 'R'];
+const vsopOrders = [...Array(6).keys()];
+
+planets.forEach((planet) => {
+  // console.log(`\n${planet}`)
+  heliocentricKeys.forEach((k) => {
+    console.log(`\n${k}`)
+    for (o in vsopOrders) {
+      let n = (VSOP[planet][k][o] && VSOP[planet][k][o].length);
+      console.log(n);
+    }
+  });
+});
+
+// =============================================
+// TFJS: Initial VSOP calculation in tensorflow =~
+// =============================================
+
+// const foo = tf.range(1, 65, 1).reshape([4, 4, 4]);
+// const bar = tf.tensor1d([0, 0, 1, 1]);
+// const baz = tf.tensor1d([0, 1, 0, 0]);
+// const pow = foo.slice([0, 0, 3], [-1, -1, 1]) //.print()
+// const bc = foo.slice([0, 0, 1], [-1, -1, 2]) //.print()
+// const a = foo.slice([0, 0, 0], [-1, -1, 1]) //.print()
+
+// tf.mul(a, bc).print()
+// tf.mul(a, bc).sum([2]).print()
+
+// =============================================
+// VSOP record counts 
+// (note: some single records are missing for [4,5])
+// =============================================
+
+// { "mercury": { "L": 3149, "B":1603, "R":2371 } },
+// { "venus": { "L": 736, "B":402, "R":572 } },
+// { "earth": { "L": 1184, "B":402, "R":978 } },
+// { "mars": { "L": 3035, "B":984, "R":2381 } },
+// { "jupiter": { "L": 1690, "B":500, "R":1435 } },
+// { "saturn": { "L": 2999, "B":947, "R":2419 } },
+// { "uranus": { "L": 2433, "B":497, "R":2343 } },
+// { "neptune": { "L": 842, "B":238, "R":939 } },
+
